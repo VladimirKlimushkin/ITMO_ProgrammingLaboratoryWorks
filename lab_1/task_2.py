dfa3c53b41f8e492d3976bfc5605549c9db4adb5
Владимир Климушкин <133119515+VladimirKlimushkin@users.noreply.github.com>
2026-09-21 02:15:28 +0300

    Update task_2.py

--- lab_1/task_2.py
+++ lab_1/task_2.py
@@ -1,10 +1,18 @@
 a = 1000
 b = a
-c = int("1000")
+c = None
 
 print("Типы:", type(a), type(b), type(c)) # int, int, int
 print("Идентификаторы:", id(a), id(b), id(c)) # id каждого из объектов
 print("a == b:", a == b) # a == b: True
 print("a is b:", a is b) # a is b: True
-print("a == c:", a == c) # a == c:
-print("a is c:", a is c)
+print("a == c:", a == c) # a == c: True
+print("a is c:", a is c) # a is c: False
+print("c is None", c is None)
+
+
+first = "python"
+second = "py" + "thon"
+
+print(first == second)
+print(first is second)
